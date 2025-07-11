d00b18e9f715c203f9aafb930131b4d4d5174af9
beauspot <42305168+beauspot@users.noreply.github.com>
2025-07-11 21:02:05 +0100

    added the helmet feature to the main entery point.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from '@nestjs/config';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   // get configuration instance
   const configService = app.get(ConfigService);
