b4e9ae9a13fb45d788adee83cd2e144df01ab0c5
beauspot <42305168+beauspot@users.noreply.github.com>
2025-07-12 12:24:04 +0100

    testing service

--- src/user/auth/auth.service.spec.ts
+++ src/user/auth/auth.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        PrismaService,
+        ConfigService
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
